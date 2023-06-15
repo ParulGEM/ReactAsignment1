b08a1956c1e1f931957cfbcdfe397201261b3ed5
Parul Sahni <Parul.Sahni@Geminisolutions.com>
2023-06-15 09:02:37 +0530

    initial commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,13 +4,16 @@ export default function Table(prop) {
   return (
     <div>
       <table className="table mt-3 mx-3 table-bordered border-warning">
-        <thead className="table-warning">
+        {
+        (prop.data.length!=0 )?
+        (<thead className="table-warning">
           <tr>
             <th scope="col">Item No</th>
             <th scope="col">Item</th>
            
           </tr>
-        </thead>
+        </thead>):""
+        } 
         <tbody>
           {prop.data.map((ele) => {
             return(
